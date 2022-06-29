862699348c60a2bf03f5511e297b152f46bd6aa4
Juan Lo <lovedrones@gmail.com>
2022-06-29 18:46:27 -0500

    local storage

--- script.js
+++ script.js
@@ -37,10 +37,12 @@ function lightMode() {
 
 function switchTheme(event) {
     if (event.target.checked){
-        document.documentElement.setAttribute('data-theme',  'dark');
+        document.documentElement.setAttribute('data-theme',  'dark'); 
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 
@@ -50,4 +52,16 @@ function switchTheme(event) {
 // Event Listener
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local Storage For Theme 
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (current === 'dark') {
+        toggleSwitch.check = true;
+        darkMode();
+    }
+};
 
